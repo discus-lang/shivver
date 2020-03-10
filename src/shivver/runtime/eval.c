a96e0f67b1c1d869c5664337ebf5eae2b8242252
Ben Lippmeier <benl@ouroborus.net>
2020-03-10 23:03:08 +1100

    eval: do prim eval for nats

--- src/shivver/runtime/eval.c
+++ src/shivver/runtime/eval.c
@@ -9,6 +9,8 @@ reqeval (bool prop, char* message)
 }
 
 
+
+
 obj_t*  shivver_eval (obj_t* oEnv, obj_t* oExp)
 {
         obj_t*  oRes     = 0;
@@ -17,57 +19,95 @@ obj_t*  shivver_eval (obj_t* oEnv, obj_t* oExp)
 }
 
 
+size_t  shivver_eval_takeNat
+        ( obj_t*        obj)
+{
+        switch(xObj_tag(obj))
+        { case TAG_NATA:
+                return xNatA(obj);
+        }
+        shivver_fail("not a nat");
+}
+
+static inline size_t
+takeNat (obj_t* obj)
+{       return shivver_eval_takeNat(obj);
+}
+
+
+void    shivver_evalPrim
+        ( size_t        nArity
+        , obj_t**       osRes
+        , size_t        tag
+        , obj_t**       osArg)
+{
+        switch(tag) {
+         case PRIM_NAT_ADD:
+         {      osRes[0] = aNatA(takeNat(osArg[0]) + takeNat(osArg[1]));
+                return;
+         }
+
+         case PRIM_NAT_SUB:
+         {      osRes[0] = aNatA(takeNat(osArg[0]) - takeNat(osArg[1]));
+                return;
+         }
+
+         case PRIM_NAT_MUL:
+         {      osRes[0] = aNatA(takeNat(osArg[0]) * takeNat(osArg[1]));
+                return;
+         }
+
+         case PRIM_NAT_DIV:
+         {      osRes[0] = aNatA(takeNat(osArg[0]) / takeNat(osArg[1]));
+                return;
+         }
+
+         default:
+                shivver_fail("invalid prim");
+        }
+}
+
+
 void    shivver_evalN
-        ( size_t  nArity        // Number of values we expect from evaluation.
-        , obj_t** osRes         // Array of pointers to fill with results.
-        , obj_t*  oEnv          // Evaluation environment.
-        , obj_t*  oExp)         // Term expression to evaluate.
+        ( size_t        nArity  // Number of values we expect from evaluation.
+        , obj_t**       osRes   // Array of pointers to fill with results.
+        , obj_t*        oEnv    // Evaluation environment.
+        , obj_t*        oExp)   // Term expression to evaluate.
 { again:
         switch(xObj_tag(oExp))
         {
           // atomic -------------------------------------------------
           case TAG_VARA:
-          {     reqeval ( nArity == 1
-                        , "eval arity for variable must be one");
-
+          {     reqeval (nArity == 1,   "eval arity for variable must be one");
                 obj_t* oRes
                  = shivver_resolveT
                         (oEnv, xVarA_name(oExp), xVarA_bump(oExp));
 
-                reqeval ( oRes != 0
-                        , "variable out of scope");
-
+                reqeval ( oRes != 0,    "variable out of scope");
                 osRes[0] = oRes;
                 return;
           }
 
           case TAG_SYMA:
-          {     reqeval ( nArity == 1
-                        , "eval arity for symbol must be one");
-
+          {     reqeval (nArity == 1,   "eval arity for symbol must be one");
                 osRes[0] = oExp;
                 return;
           }
 
           case TAG_PRMA:
-          {     reqeval ( nArity == 1
-                        , "eval arity for primitive must be one");
-
+          {     reqeval (nArity == 1,   "eval arity for primitive must be one");
                 osRes[0] = oExp;
                 return;
           }
 
           case TAG_PRZA:
-          {     reqeval ( nArity == 1
-                        , "eval arity for primitive must be one");
-
+          {     reqeval (nArity == 1,   "eval arity for primitive must be one");
                 osRes[0] = oExp;
                 return;
           }
 
           case TAG_NATA:
-          {     reqeval ( nArity == 1
-                        , "eval arity for literal must be one");
+          {     reqeval (nArity == 1,   "eval arity for literal must be one");
                 osRes[0] = oExp;
                 return;
           }
@@ -75,20 +115,15 @@ void    shivver_evalN
           // hot ----------------------------------------------------
           case TAG_MMMH:
           {     size_t nLen = xMmmH_len(oExp);
-                reqeval ( nLen == nArity
-                        , "eval arity does not match vector length");
-
+                reqeval (nLen == nArity, "eval arity does not match vector length");
                 for (size_t i = 0; i < nLen; i++)
                         shivver_evalN(1, osRes + i, oEnv, xMmmH_arg(oExp, i));
-
                 return;
           }
 
           case TAG_ABSH:
           {     // Convert abstractions into closures.
-                reqeval ( nArity == 1
-                        , "eval arity for abstraction must be one");
-
+                reqeval ( nArity == 1,  "eval arity for abstraction must be one");
                 obj_t* oAbs     = oExp;
                 obj_t* oBody    = xAbsH_body(oAbs);
                 osRes[0]        = aCloH ( xAbsH_len(oExp)
@@ -105,7 +140,8 @@ void    shivver_evalN
                 obj_t* oHeadV   = 0;
                 shivver_evalN (1, &oHeadV, oEnv, oHead);
 
-                switch(xObj_tag(oHeadV))
+                uint32_t tag    = xObj_tag(oHeadV);
+                switch(tag)
                 { case TAG_SYMA:
                   {     // For applications of symbols,
                         // evaluate all the arguments and rebuild the application.
@@ -115,6 +151,20 @@ void    shivver_evalN
                         return;
                   }
 
+                  case TAG_PRZA:
+                  {     uint32_t pTag   = xPrzA_tag(oHeadV);
+                        size_t nParams  = shivver_prim_tag_args(pTag);
+
+                        obj_t* osArgs[nParams];
+                        shivver_evalN(nParams, osArgs, oEnv, oArg);
+
+                        size_t nResults = shivver_prim_tag_results(pTag);
+                        reqeval ( nArity == nResults
+                                , "eval arity does not match number of prim op results");
+
+                        shivver_evalPrim (nArity, osRes, pTag, osArgs);
+                        return;
+                  }
 
                   case TAG_CLOH:
                   {     // For applications of closures,
@@ -165,13 +215,33 @@ void    shivver_evalN
                         return;
                   }
 
+                  case TAG_PRZA:
+                  {     uint32_t pTag   = xPrzA_tag(oHeadV);
+                        size_t nParams  = shivver_prim_tag_args(pTag);
+                        reqeval ( nParams == nArgs
+                                , "number of parameters must match number of arguments");
+
+                        // Evaluate all the arguments.
+                        obj_t* osArgs[nArgs];
+                        for(size_t i = 0; i < nArgs; i++)
+                        {       obj_t* oArg = xApsH_arg(oAps, i);
+                                shivver_evalN(1, osArgs + i, oEnv, oArg);
+                        }
+
+                        // Apply the primitive.
+                        size_t nResults = shivver_prim_tag_results(pTag);
+                        reqeval ( nArity == nResults
+                                , "eval arity does not match number of prim op results");
+                        shivver_evalPrim (nArity, osRes, pTag, osArgs);
+                        return;
+                  }
+
                   case TAG_CLOH:
                   {     // For applications of closures,
                         // evaluate the arguments, add them to environment and continue
                         // with the body expresssion.
                         obj_t* oClo     = oHeadV;
                         size_t nParams  = xCloH_len(oClo);
-                        size_t nArgs    = xApsH_len(oAps);
                         reqeval ( nParams == nArgs
                                 , "number of parameters must match number of arguments");
 
@@ -208,30 +278,25 @@ void    shivver_evalN
 
           // static  --------------------------------------------------
           case TAG_VART:
-          {     reqeval ( nArity == 1
-                        , "eval arity for variable must be one");
+          {     reqeval ( nArity == 1,  "eval arity for variable must be one");
 
                 obj_t* oRes
                  = shivver_resolveT
                         (oEnv, xVarT_name(oExp), xVarT_bump(oExp));
-
-                reqeval ( oRes != 0
-                        , "variable out of scope");
+                reqeval ( oRes != 0,    "variable out of scope");
 
                 osRes[0] = oRes;
                 return;
         }
 
           case TAG_SYMT:
-          {     reqeval ( nArity == 1
-                        , "eval arity for symbol must be one");
+          {     reqeval ( nArity == 1,  "eval arity for symbol must be one");
                 osRes[0] = oExp;
                 return;
           }
 
           case TAG_PRMT:
-          {     reqeval ( nArity == 1
-                        , "eval arity for primitive must be one");
+          {     reqeval ( nArity == 1,  "eval arity for primitive must be one");
                 osRes[0] = oExp;
                 return;
           }
